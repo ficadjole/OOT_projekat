1b0c3718c459ad79f05e25607d392b7248779c0f
ficadjole <velemirfilip@gmail.com>
2024-05-17 16:49:39 +0200

    DataGrid

--- OOT_Kursevi/OOT_Kursevi/Kurs.cs
+++ OOT_Kursevi/OOT_Kursevi/Kurs.cs
@@ -29,13 +29,14 @@ namespace OOT_Kursevi
             }
         }
 
-        public Kurs(string naziv, int cena, string vrsta, bool dostupnost, int iD, string opis, string putanja_slike)
+        public Kurs(int iD,string naziv, int cena, string vrsta, string opis, bool dostupnost,string putanja_slike)
         {
             this.naziv = naziv;
             this.cena = cena;
             this.vrsta = vrsta;
             this.dostupnost = dostupnost;
             id = iD;
+            this.opis = opis;
             slika = new Image();
             slika.Source = new BitmapImage(new Uri(putanja_slike));
         }
