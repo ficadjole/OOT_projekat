e0340b0a30765ae0369f95cb3339e5614c4885b8
ficadjole <velemirfilip@gmail.com>
2024-05-17 17:08:21 +0200

    nesto

--- OOT_Kursevi/OOT_Kursevi/obj/Debug/net6.0-windows/OOT_Kursevi.AssemblyInfo.cs
+++ OOT_Kursevi/OOT_Kursevi/obj/Debug/net6.0-windows/OOT_Kursevi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOT_Kursevi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1412834fc8f3fd0f1a9f3242e19420f1d16abeb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0c3718c459ad79f05e25607d392b7248779c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOT_Kursevi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOT_Kursevi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
