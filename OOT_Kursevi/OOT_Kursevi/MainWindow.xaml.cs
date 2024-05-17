6f9bdd6b1aeb67e7460d9f9a7bd56d9e46f30c3d
ficadjole <velemirfilip@gmail.com>
2024-05-17 18:17:34 +0200

    Tab1_Main drag drop

--- OOT_Kursevi/OOT_Kursevi/MainWindow.xaml.cs
+++ OOT_Kursevi/OOT_Kursevi/MainWindow.xaml.cs
@@ -19,15 +19,18 @@ namespace OOT_Kursevi
     {
 
         private ObservableCollection<Kurs> kursevi = new ObservableCollection<Kurs>();
-        private ObservableCollection<Kurs> kursevi2 = new ObservableCollection<Kurs>();
+        private ObservableCollection<Kurs> kursevi_nedostupni = new ObservableCollection<Kurs>();
 
         public MainWindow()
         {
             InitializeComponent();
-            kursevi.Add(new Kurs(123,"Ronjenje",3000,"Sport","Ronjenje na dah",true,"C:\\Users\\win 10\\Desktop\\SnapShot1.jpg"));
-            kursevi2.Add(new Kurs(123, "Ronjenje", 3000, "Sport", "Ronjenje na dah", false, "C:\\Users\\win 10\\Desktop\\SnapShot1.jpg"));
+            kursevi.Add(new Kurs(1,"Ronjenje",3000,"Sport","Ronjenje na dah",true, ".\\OOT_Kursevi\\slike\\C.webp"));
+            kursevi.Add(new Kurs(2,"Teretana",5200,"Sport","Personalni trening",true,".\\OOT_Kursevi\\slike\\gym.jpg"));
+            
+            kursevi_nedostupni.Add(new Kurs(2, "Pravljenje paste", 2500, "Hrana", "Italijanska receptura", false, ".\\OOT_Kursevi\\slike\\C.webp"));
+            
             dtGrid_dostupni.ItemsSource = kursevi;
-            dtGrid_nedostupni.ItemsSource = kursevi2;
+            dtGrid_nedostupni.ItemsSource = kursevi_nedostupni;
         }
 
         private void BTN_DodajClick(object sender, RoutedEventArgs e)
@@ -36,6 +39,26 @@ namespace OOT_Kursevi
         }
 
         private void BTN_PotvrdiClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dtGrid_dostupni_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void dtGrid_dostupni_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void dtGrid_nedostupni_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void dtGrid_nedostupni_Drop(object sender, DragEventArgs e)
         {
 
         }
